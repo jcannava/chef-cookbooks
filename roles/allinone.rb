bc7c2df9f9b03acb420d7a690298705014d63df7
jcannava <jason@cannavale.com>
2013-10-17 13:24:23 -0500

    removing collectd from allinone

--- roles/allinone.rb
+++ roles/allinone.rb
@@ -4,10 +4,3 @@ run_list(
   "role[single-controller]",
   "role[single-compute]"
 )
-
-#
-# TODO(breu): These are commented out for now
-#
-# "role[collectd-client]",
-# "role[collectd-server]",
-# "role[graphite]"
